8d4d117c8e1ced0fd7e4b3386a5bd96e167a894b
Sambhavi PD <sambhavi@golittlebig.com>
2022-09-28 16:59:43 +0530

    Added validation for fields in Classes and changed str to Author in Book classes

--- app/schemas.py
+++ app/schemas.py
@@ -1,32 +1,32 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
 
 class Author(BaseModel):
     id: int
     name: str
-    genre: list[str]
+    genre: list[str] = Field(min_items=1, max_items=5)
 
 class AuthorCreate(BaseModel):
     name: str
-    genre: list[str]
+    genre: list[str] = Field(min_items=1, max_items=5)
 
 class AuthorUpdate(BaseModel):
-    genre: list[str]
+    genre: list[str] = Field(min_items=1, max_items=5)
 
 class Book(BaseModel):
     id: int
-    title: str
-    authors: list[Author]
-    rating: float
-    genre: list[str]
-    numberofpages: int
+    title: str = Field(description="Title of the book", max_length=200)
+    authors: list[Author] = Field(min_items=1)
+    rating: float = Field(gt=0, description="Rating must be greater than zero")
+    genre: list[str] = Field(min_items=1, max_items=5)
+    numberofpages: int = Field(gt=0, description="Number of pages must be greater than zero")
 
 class BookCreate(BaseModel):
-    title: str
+    title: str = Field(description="Title of the book", max_length=200)
     authors: list[Author]
-    rating: float
-    genre: list[str]
-    numberofpages: int
+    rating: float = Field(gt=0, description="Rating must be greater than zero")
+    genre: list[str] = Field(min_items=1, max_items=5)
+    numberofpages: int = Field(gt=0, description="Number of pages must be greater than zero")
 
 class BookUpdate(BaseModel):
-    title: str
-    genre: list[str]
+    title: str = Field(description="Title of the book", max_length=200)
+    genre: list[str] = Field(min_items=1, max_items=5)
